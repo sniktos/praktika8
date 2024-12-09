9996dd9296aa58c72477227f721cb2f6049d079f
cntaf <gsdgs@fue.re>
2024-12-10 01:03:57 +0700

    итерация1 - Объявлены все классы. Все основные методы в виде заглушек с pass или return "образец"

---
+++ main.py
@@ -0,0 +1,38 @@
+class TextProcessor:
+    def __init__(self):
+        self.data = None
+        self.result = None
+
+    def input_data(self):
+        pass
+
+    def generate_data(self):
+        pass
+
+    def process_data(self):
+        pass
+
+    def reset_result(self):
+        pass
+
+    def get_result(self):
+        return "Sample result"
+
+
+class Menu:
+    def __init__(self):
+        self.processor = TextProcessor()
+
+    def display_menu(self):
+        pass
+
+    def handle_choice(self, choice):
+        pass
+
+    def run(self):
+        pass
+
+
+if name == "__main__":
+    menu = Menu()
+    menu.run()
